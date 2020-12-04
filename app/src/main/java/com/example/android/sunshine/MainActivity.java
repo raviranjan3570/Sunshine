9eb85f7f24dd7906486457ffb458cfbf63e6e4e2
Ravi Ranjan <raviranjan3570@gmail.com>
2020-12-04 21:51:45 +0530

    updated the icon pack

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -23,6 +23,7 @@ import com.example.android.sunshine.activities.SettingsActivity;
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
 import com.example.android.sunshine.sync.SunshineSyncUtils;
+import com.example.android.sunshine.utilities.NotificationUtils;
 
 import java.util.Objects;
 
@@ -130,6 +131,7 @@ public class MainActivity extends AppCompatActivity implements
         LoaderManager.getInstance(this).initLoader(FORECAST_LOADER_ID, null, this);
 
         SunshineSyncUtils.initialize(this);
+        NotificationUtils.notifyUserOfNewWeather(this);
     }
 
     /**
--- app/src/main/java/com/example/android/sunshine/utilities/SunshineWeatherUtils.java
+++ app/src/main/java/com/example/android/sunshine/utilities/SunshineWeatherUtils.java
@@ -366,36 +366,36 @@ public final class SunshineWeatherUtils {
          * Based on weather code data for Open Weather Map.
          */
         if (weatherId >= 200 && weatherId <= 232) {
-            return R.drawable.art_storm;
+            return R.drawable.ic_storm;
         } else if (weatherId >= 300 && weatherId <= 321) {
-            return R.drawable.art_light_rain;
+            return R.drawable.ic_light_rain;
         } else if (weatherId >= 500 && weatherId <= 504) {
-            return R.drawable.art_rain;
+            return R.drawable.ic_rain;
         } else if (weatherId == 511) {
-            return R.drawable.art_snow;
+            return R.drawable.ic_snow;
         } else if (weatherId >= 520 && weatherId <= 531) {
-            return R.drawable.art_rain;
+            return R.drawable.ic_rain;
         } else if (weatherId >= 600 && weatherId <= 622) {
-            return R.drawable.art_snow;
+            return R.drawable.ic_snow;
         } else if (weatherId >= 701 && weatherId <= 761) {
-            return R.drawable.art_fog;
+            return R.drawable.ic_fog;
         } else if (weatherId == 771 || weatherId == 781) {
-            return R.drawable.art_storm;
+            return R.drawable.ic_storm;
         } else if (weatherId == 800) {
-            return R.drawable.art_clear;
+            return R.drawable.ic_clear;
         } else if (weatherId == 801) {
-            return R.drawable.art_light_clouds;
+            return R.drawable.ic_light_clouds;
         } else if (weatherId >= 802 && weatherId <= 804) {
-            return R.drawable.art_clouds;
+            return R.drawable.ic_cloudy;
         } else if (weatherId >= 900 && weatherId <= 906) {
-            return R.drawable.art_storm;
+            return R.drawable.ic_storm;
         } else if (weatherId >= 958 && weatherId <= 962) {
-            return R.drawable.art_storm;
+            return R.drawable.ic_storm;
         } else if (weatherId >= 951 && weatherId <= 957) {
-            return R.drawable.art_clear;
+            return R.drawable.ic_clear;
         }
 
         Log.e(LOG_TAG, "Unknown Weather: " + weatherId);
-        return R.drawable.art_storm;
+        return R.drawable.ic_storm;
     }
 }
